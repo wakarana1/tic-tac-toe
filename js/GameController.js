271d2b9eb3dc0d3b45ff26fec0b21323dd0bb287
wakarana1 <wu.chris@outlook.com>
2015-05-15 17:58:37 -0700

    fixed firebase implementation

--- js/GameController.js
+++ js/GameController.js
@@ -2,78 +2,99 @@ angular
 	.module("TicTacToe")
 	.controller("GameController", GameController);
 
-	GameController.$inject = ['$scope', '$firebaseObject', '$firebaseArray'];
+	GameController.$inject = ['$scope', '$firebaseObject'];
 
-function GameController($scope, $firebaseObject, $firebaseArray) {
+function GameController($scope, $firebaseObject) {
 
-	var ref = new Firebase("https://wakarana1-tic-tac-toe.firebaseio.com/");
-	
-	$scope.board = $firebaseArray(ref.child("board"));
+	var ref = new Firebase("https://wakarana1-tic-tac-toe.firebaseio.com");
+	$firebaseObject(ref).$bindTo($scope, "game").then(function(){
+		if ($scope.game.players.player1.name === ""){
+			$scope.game.players.player1.name = "X";
+			thisPlayer = -1;
+		}else if ($scope.game.players.player2.name === "") {
+			$scope.game.players.player2.name = "Y";
+			thisPlayer = 1;
+		}else {
+			alert("SPECTATOR");
+		}
+	});
 
-	$scope.player1 = -1;
-	$scope.player2 = 1;
+	// $scope.game.player1 = -1;
+	// $scope.game.player2 = 1;
+	var thisPlayer;
 
-	var currentPlayer = -1;
 
+	//code runs when player clicks on a cell
 	$scope.playerMove = function (cellIndex) {
+			if (thisPlayer == $scope.game.currentPlayer) {
 
-		$scope.score1 = 0;       
-		$scope.score2 = 0;
+				// $scope.game.players.player1.wins = 0;       
+				// $scope.game.players.player2.wins = 0;
+				if ($scope.game.board[cellIndex] !== 0) {
+					return;
+				}
+				$scope.game.board[cellIndex] = $scope.game.currentPlayer;
+				console.log('clicky clicky');
+				getWinner();
 
-		if ($scope.board[cellIndex].$value !== 0) {
-			return;
-		}
-
-		$scope.board[cellIndex].$value = currentPlayer;
-
-		$scope.board.$save(cellIndex);
-
-		if (currentPlayer === -1) {
-			currentPlayer = 1;
-		}else{
-			currentPlayer = -1;
-		}
+				if ($scope.game.currentPlayer === -1) {
+					$scope.game.currentPlayer = 1;
+				}else{
+					$scope.game.currentPlayer = -1;
+				}
+			}else {
+				return ;
+			}
 	};
 
+// checks who wins and assigns points to x/y
 	function checkWinner(sum) {
 	   if ( sum === -3) {
-			$scope.score1++;
+			$scope.game.players.player1.wins++;
 			return -1;
 	   }else if (sum === 3) {
-			$scope.score2++;
-			return 1;
+			$scope.game.players.player2.wins++;
+			return 1;			
 	   }else {
-	   		return 0;
+	   		return;
 	   }
 	}
 
+	// resets the game
+	$scope.resetGame = function(){
+		$scope.game.board = [0,0,0,0,0,0,0,0];
+		};
+
+
+ // function to figure out the winner
 	function getWinner() {
-	
+
 		var sum = 0;
 		var winner = 0;
 
 		for(var row = 0; row < 3; row++){
-	    	sum = $scope.board[row * 3].$value + $scope.board[row * 3 + 1].$value + $scope.board[row * 3 + 2].$value;
+	    	sum = $scope.game.board[row * 3] + $scope.game.board[row * 3 + 1] + $scope.game.board[row * 3 + 2];
 	    	winner = checkWinner(sum);
 	    	if ( winner ) {
+	    		console.log("Winner!");
 	    		return winner; 
 	    	}
 	   }
 
 		for(var col = 0; col < 3; col++){
-			sum = $scope.board[col].$value + $scope.board[col + 3].$value + $scope.board[col + 6].$value;
+			sum = $scope.game.board[col] + $scope.game.board[col + 3] + $scope.game.board[col + 6];
 			winner = checkWinner(sum);
 			if ( winner ) {
 				return winner;
 			}
 	   }
 
-		sum = $scope.board[0].$value + $scope.board[4].$value + $scope.board[8].$value;
+		sum = $scope.game.board[0] + $scope.game.board[4] + $scope.game.board[8];
 		winner = checkWinner(sum);
 			if ( winner ) {
 				return winner; }
 
-		sum = $scope.board[6].$value + $scope.board[4].$value + $scope.board[2].$value;
+		sum = $scope.game.board[6] + $scope.game.board[4] + $scope.game.board[2];
 		winner = checkWinner(sum);
 		if ( winner ) {
 			return winner; 
@@ -81,5 +102,4 @@ function GameController($scope, $firebaseObject, $firebaseArray) {
 
 		return 0;
 	}
-
 }
